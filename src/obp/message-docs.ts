25a6ef8bc1ddc12aeb8a30cec147249d0003d48e
Marko Milić <marko@tesobe.com>
2024-02-12 16:53:29 +0100

    feature/Loading resource/message docs info

--- src/obp/message-docs.ts
+++ src/obp/message-docs.ts
@@ -24,6 +24,20 @@ export async function cacheDoc(messageDocsCache: any): Promise<any> {
   const messageDocs = await connectors.reduce(async (agroup: any, connector: any) => {
     const group = await agroup
     const docs = await getOBPMessageDocs(connector)
+    const logMessage = `Loading message docs { connector: ${ connector } }`
+    console.log(logMessage)
+    // 1. Select the div element using the id property
+    const spinner = document.getElementById("loading-api-spinner");
+    // 2. Select the div element using the id property
+    let p = document.getElementById("loading-api-info");
+    // 3. Add the text content
+    if(p !== null) {
+      p.textContent = logMessage;
+    } else {
+      p = document.createElement("p")
+    }
+    // 4. Append the p element to the div element
+    spinner?.appendChild(p);
     if (!Object.keys(docs).includes('code')) {
       group[connector] = getGroupedMessageDocs(docs)
     }
