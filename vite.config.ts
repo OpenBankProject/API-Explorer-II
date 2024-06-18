52916d31a8195421789f118985f1df0f04b3124e
nemo <nemozak1@gmail.com>
2024-06-18 17:00:28 +0100

    bugfix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,7 @@ import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
-  const env = loadEnv(mode, process.cwd(), '');
-
+  
   plugins: [
     vue(), vueJsx(),
     AutoImport({
